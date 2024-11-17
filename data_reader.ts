45541e3a695b6ffb394c3382f3657fb6450eea02
Ryan Deivert <rdeivert@icloud.com>
2024-11-16 19:07:55 -0800

    added scrollbar

--- data_reader.ts
+++ data_reader.ts
@@ -1,8 +1,14 @@
 const fillData = () => {
-    return "hello world";
+    let result = "";
+    for (let i = 0; i < 10000; i++) {
+        result += "hello world";
+    }
+
+    return result;
 };
 
 console.log("hello world");
 
 const data_bar = document.getElementById("scroll_box");
+data_bar.innerHTML = fillData();
 
